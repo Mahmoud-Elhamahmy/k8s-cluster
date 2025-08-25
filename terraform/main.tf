39cb4f93fd685ddae1b76320d8d2932d77377409
Mahmoud-Elhamahmy <mahmoudelhamahmy87@gmail.com>
2025-08-25 17:23:29 +0300

    add project to git

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,11 +4,14 @@ provider "aws" { region = var.region }
 resource "aws_vpc" "this" {
   cidr_block = "10.0.0.0/16"
 }
+# Subnet 
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.this.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"   
 }
+
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.this.id
 }
