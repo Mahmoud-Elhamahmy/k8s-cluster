6b8e196c6fc0ae7cbc8e996746ec8ba3d004a927
Mahmoud Elhamahmy <147988907+Mahmoud-Elhamahmy@users.noreply.github.com>
2025-08-31 13:56:37 +0300

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,6 +1,7 @@
 output "master_public_ip" {
   value = aws_instance.master.public_ip
 }
+
 output "worker_public_ips" {
-  value = [for w in aws_instance.workers : w.public_ip]
+  value = [for instance in aws_instance.worker : instance.public_ip]
 }
