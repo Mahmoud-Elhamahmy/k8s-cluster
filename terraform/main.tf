d3c6b9e2c620913a2bceafe2f903e45c5b5c12b2
Mahmoud Elhamahmy <147988907+Mahmoud-Elhamahmy@users.noreply.github.com>
2025-08-31 14:27:22 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -91,15 +91,6 @@ resource "aws_security_group" "k8s" {
 }
 
 ############################
-# Key Pair                 #
-############################
-
-resource "aws_key_pair" "deployer" {
-  key_name   = var.key_name
-  public_key = file("~/.ssh/id_rsa.pub")
-}
-
-############################
 # IAM Role for SSM         #
 ############################
 
